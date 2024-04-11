4e0ab4f8bad2cb9a07d3877903e99cae4123978a
yusukehirao <yusukehirao@me.com>
2024-04-11 22:23:22 +0900

    Update `readtext`

--- packages/@d-zero/readtext/src/read-list.ts
+++ packages/@d-zero/readtext/src/read-list.ts
@@ -1,12 +1,17 @@
+import type { KeyValue } from './types.js';
+
 import fs from 'node:fs/promises';
 
 /**
- * Reads a text file and returns an array of non-comment lines.
+ * Reads a list of values from a file.
  *
- * @param filePath - The path to the text file.
- * @returns An array of non-comment lines from the text file.
+ * @param filePath - The path to the file.
+ * @param keyValue - Optional flag to indicate whether the file contains key-value pairs.
+ * @returns A promise that resolves to an array of values from the file.
  */
-export async function readList(filePath: string) {
+export async function readList(filePath: string): Promise<string[]>;
+export async function readList(filePath: string, keyValue: true): Promise<KeyValue[]>;
+export async function readList(filePath: string, keyValue?: true): Promise<unknown[]> {
 	const fileContent = await fs.readFile(filePath, 'utf8');
 	const lines = fileContent.split('\n');
 
@@ -19,5 +24,12 @@ export async function readList(filePath: string) {
 	// Remove comments
 	const nonCommentLines = nonEmptyLines.filter((line) => !line.startsWith('#'));
 
-	return nonCommentLines;
+	if (!keyValue) {
+		return nonCommentLines;
+	}
+
+	return nonCommentLines.map((line) => {
+		const [key, value] = line.split(/\s+/);
+		return { key, value };
+	});
 }
--- packages/@d-zero/readtext/src/types.ts
+++ packages/@d-zero/readtext/src/types.ts
@@ -1,3 +1,8 @@
 export type TextType = 'list' | 'grid';
 
 export type Separator = '  ' | '\t' | ',' | RegExp;
+
+export type KeyValue = {
+	key: string;
+	value: string;
+};
