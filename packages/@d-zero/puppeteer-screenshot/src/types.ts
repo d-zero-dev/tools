6ddf6faf98dd43e8a8d16fd61baaa36630875899
YusukeHirao <hirao@d-zero.co.jp>
2024-10-19 15:17:20 +0900

    chore(puppeteer-screenshot): mark `@deprecated` to `ScreenshotListener` type

--- packages/@d-zero/puppeteer-screenshot/src/types.ts
+++ packages/@d-zero/puppeteer-screenshot/src/types.ts
@@ -21,6 +21,9 @@ export type ScreenshotPhase = {
 	getDOMEnd: { name: string; dom: string };
 } & PageScanPhase;
 
+/**
+ * @deprecated
+ */
 export type ScreenshotListener = GeneralListener<ScreenshotPhase>;
 
 /**
