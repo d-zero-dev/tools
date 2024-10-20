68b374556c36b3ae285438edba9cb78215be45e6
YusukeHirao <hirao@d-zero.co.jp>
2024-10-20 18:49:40 +0900

    feat(puppeteer-scroll): add `accurate` option to `scrollAllOver` function

--- packages/@d-zero/puppeteer-scroll/src/scroll-all-over.ts
+++ packages/@d-zero/puppeteer-scroll/src/scroll-all-over.ts
@@ -1,8 +1,11 @@
 import type { Page } from 'puppeteer';
 
+import { delay } from '@d-zero/shared/delay';
+
 export type Options = {
 	distance?: number;
 	interval?: number;
+	accurate?: boolean;
 	logger?: (scrollY: number, scrollHeight: number, message: string) => void;
 };
 
@@ -13,9 +16,45 @@ export type Options = {
  * @param options - Optional parameters for scrolling.
  * @param options.distance - The distance to scroll on each iteration (default: 100).
  * @param options.interval - The interval between each scroll iteration in milliseconds (default: 300).
+ * @param options.accurate
  * @param options.logger - A function that logs messages.
  */
 export async function scrollAllOver(page: Page, options?: Options) {
+	const interval = options?.interval ?? 300;
+
+	if (options?.accurate) {
+		let currentScrollY = 0;
+		let scrollHeight = await page.evaluate(() => document.body.scrollHeight);
+
+		while (currentScrollY < scrollHeight) {
+			[currentScrollY, scrollHeight] = await page.evaluate(() => {
+				// Move the scroll position to the bottom of the page.
+				globalThis.scrollBy(0, document.documentElement.clientHeight);
+				// Return the current scroll position.
+				return [
+					globalThis.scrollY + globalThis.innerHeight,
+					document.body.scrollHeight,
+				] as const;
+			});
+			options?.logger?.(currentScrollY, scrollHeight, 'Scrolling');
+			await delay(interval);
+		}
+
+		options?.logger?.(currentScrollY, scrollHeight, 'End of page');
+
+		await page.evaluate(() => {
+			// Move the scroll position to the top of the page.
+			globalThis.scrollTo(0, 0);
+		});
+		await delay(400);
+
+		options?.logger?.(currentScrollY, scrollHeight, 'End of page');
+
+		await delay(400);
+
+		return;
+	}
+
 	options?.logger?.(Number.NaN, Number.NaN, 'Scrolling (Inaccurate Mode)');
 
 	await page.evaluate(
@@ -36,6 +75,6 @@ export async function scrollAllOver(page: Page, options?: Options) {
 			});
 		},
 		options?.distance,
-		options?.interval ?? 300,
+		interval,
 	);
 }
