bf8df24271dd3c8da23cd3f8b459db2af1c39048
YusukeHirao <hirao@d-zero.co.jp>
2024-10-30 21:53:24 +0900

    feat(puppeteer-scroll): drop `accurate` option
    BREAKING CHANGE:
    drop `accurate` option

--- packages/@d-zero/puppeteer-scroll/src/scroll-all-over.ts
+++ packages/@d-zero/puppeteer-scroll/src/scroll-all-over.ts
@@ -5,7 +5,6 @@ import { delay } from '@d-zero/shared/delay';
 export type Options = {
 	distance?: number;
 	interval?: number;
-	accurate?: boolean;
 	logger?: (scrollY: number, scrollHeight: number, message: string) => void;
 };
 
@@ -16,65 +15,37 @@ export type Options = {
  * @param options - Optional parameters for scrolling.
  * @param options.distance - The distance to scroll on each iteration (default: 100).
  * @param options.interval - The interval between each scroll iteration in milliseconds (default: 300).
- * @param options.accurate
  * @param options.logger - A function that logs messages.
  */
 export async function scrollAllOver(page: Page, options?: Options) {
 	const interval = options?.interval ?? 300;
 
-	if (options?.accurate) {
-		let currentScrollY = 0;
-		let scrollHeight = await page.evaluate(() => document.body.scrollHeight);
+	let currentScrollY = 0;
+	let scrollHeight = await page.evaluate(() => document.body.scrollHeight);
 
-		while (currentScrollY < scrollHeight) {
-			[currentScrollY, scrollHeight] = await page.evaluate(() => {
-				// Move the scroll position to the bottom of the page.
-				globalThis.scrollBy(0, document.documentElement.clientHeight);
-				// Return the current scroll position.
-				return [
-					globalThis.scrollY + globalThis.innerHeight,
-					document.body.scrollHeight,
-				] as const;
-			});
-			options?.logger?.(currentScrollY, scrollHeight, 'Scrolling');
-			await delay(interval);
-		}
-
-		options?.logger?.(currentScrollY, scrollHeight, 'End of page');
-
-		await page.evaluate(() => {
-			// Move the scroll position to the top of the page.
-			globalThis.scrollTo(0, 0);
+	while (currentScrollY < scrollHeight) {
+		[currentScrollY, scrollHeight] = await page.evaluate(() => {
+			// Move the scroll position to the bottom of the page.
+			globalThis.scrollBy(0, document.documentElement.clientHeight);
+			// Return the current scroll position.
+			return [
+				globalThis.scrollY + globalThis.innerHeight,
+				document.body.scrollHeight,
+			] as const;
 		});
-		await delay(400);
-
-		options?.logger?.(currentScrollY, scrollHeight, 'End of page');
-
-		await delay(400);
-
-		return;
+		options?.logger?.(currentScrollY, scrollHeight, 'Scrolling');
+		await delay(interval);
 	}
 
-	options?.logger?.(Number.NaN, Number.NaN, 'Scrolling (Inaccurate Mode)');
+	options?.logger?.(currentScrollY, scrollHeight, 'End of page');
 
-	await page.evaluate(
-		async (distance, interval) => {
-			distance = distance ?? document.documentElement.clientHeight - 5;
-			await new Promise<void>((resolve) => {
-				let totalHeight = 0;
-				const timer = setInterval(() => {
-					const scrollHeight = document.body.scrollHeight;
-					window.scrollBy(0, distance);
-					totalHeight += distance;
-					if (totalHeight >= scrollHeight || totalHeight >= 100_000) {
-						clearInterval(timer);
-						window.scrollBy(0, 0);
-						resolve();
-					}
-				}, interval);
-			});
-		},
-		options?.distance,
-		interval,
-	);
+	await page.evaluate(() => {
+		// Move the scroll position to the top of the page.
+		globalThis.scrollTo(0, 0);
+	});
+	await delay(400);
+
+	options?.logger?.(currentScrollY, scrollHeight, 'End of page');
+
+	await delay(400);
 }
