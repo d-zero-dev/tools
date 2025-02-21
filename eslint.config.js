eda3cc650e51ec7b8d3d8384b12d7bd2ba0caf2c
YusukeHirao <hirao@d-zero.co.jp>
2025-02-21 20:42:23 +0900

    chore(repo): apply ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import dz from '@d-zero/eslint-config';
+
+/**
+ * @type {import('eslint').ESLint.ConfigData[]}
+ */
+export default [
+	...dz.configs.standard,
+	{
+		rules: {
+			'@typescript-eslint/ban-ts-comment': 0,
+		},
+	},
+	{
+		files: ['*.mjs', '**/*.spec.{js,mjs,ts}'],
+		rules: {
+			'import/no-extraneous-dependencies': 0,
+		},
+	},
+	{
+		files: ['.textlintrc.js'],
+		...dz.configs.commonjs,
+	},
+	{
+		ignores: ['**/dist/**/*'],
+	},
+];
